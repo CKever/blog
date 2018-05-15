ff63ff92f6f5e93dfa4a44711e76b1f93ec51b83
CKever <33261677+CKever@users.noreply.github.com>
2018-05-15 18:16:27 +0800

    blog v1.0

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3-j)8zi6&yun$h7r9&klozbi(t06svvipvfk@o%qjm=q4fq@!$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
